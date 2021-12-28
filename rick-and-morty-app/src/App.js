f41165b44164d4abdebc0e9e4993273ede2ea8c2
Salome Gomez <sgomez713@misena.edu.co>
2021-12-27 20:31:21 -0500

    app con React sobre los personajes de la series Rick and Morty

---
+++ rick-and-morty-app/src/App.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import Navbar from './components/Navbar';
+import Characters from './components/Characters';
+import Pagination from './components/Pagination';
+import Logo from './components/Logo';
+
+function App() {
+
+  const [characters, setCharacters] = useState([]);
+  const [info,setInfo] = useState({});
+
+  const initialUrl = "https://rickandmortyapi.com/api/character"
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch(error => console.error(error))
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, [])
+
+  return (
+    <>
+      <Navbar brand="Rick and Morty App" />
+
+      <div className="container mt-5">
+        <Logo />
+        <Pagination 
+          prev={info.prev} 
+          next={info.next} 
+          onPrevious={onPrevious} 
+          onNext={onNext} 
+        />
+        <Characters characters={characters} />
+        <Pagination 
+          prev={info.prev} 
+          next={info.next} 
+          onPrevious={onPrevious} 
+          onNext={onNext} 
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ rick-and-morty-app/src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div>
+            <img style={{width: "50%"}} className="mx-auto d-block" src="../images/logo.png" />
+        </div>
+    )
+}
+
+export default Logo
